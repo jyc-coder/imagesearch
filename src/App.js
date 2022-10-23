b6c91abb267fe7f1e2768e5b84831d14381d5dfa
jyc-coder <jyc4648@gmail.com>
2022-10-23 17:46:43 +0900

    #2 - infinite scroll 구현
    검색어를 입력하여 나타난 결과를 열람할때 스크롤을 내리면 지속해서 결과물을 보여주는 기능을 구현함
    
    * `useintersectionObserver` hook을 제작해서 페이지 화면이 스크롤을 내렸을때 기준이 되는 컴포넌트까지 스크롤이 내려오면 검색결과를 추가적으로 load 해서 반복 렌더링을 시키는 구조로 구현함
    
    ※ IntersectionObserver
    https://developer.mozilla.org/ko/docs/Web/API/Intersection_Observer_API
    
    * 관측 여부에 따라서 `isIntersecting`의 값을 업데이트하게 해서 `image-container`하단에 `loading ` div에 `ref`를 지정하여 해당 컴포넌트가 관측이 된다면 `isIntersectiong`이 true로 변경되며 
    `useEffect`에서 관측되었거나(isIntersectiong이 true), 아직 검색 결과가 다 나온 것이 아니거나 (현재 나온 데이터 길이가 총 검색 결과의 길이가 같이 않은 경우 isEnd) , 로딩이 끝났다면(데이터가 존재하지 않고, 에러가 발생하지 않고, 검색어가 존재하던지, 아니면 isValidating이 true이던지),
    setSIze를 사용해서 Size의 값을 1 더하게 되고, 그만큼의 검색결과 데이터가 로딩되는 원리
    
    * 로딩 중인 경우 `react-loader-spinner`를 사용해서 아이콘으로 표시되게 함

--- src/App.js
+++ src/App.js
@@ -1,52 +1,75 @@
 import './App.css';
-import { useState, useCallback } from 'react';
+import {useState, useCallback, useRef, useEffect} from 'react';
 import useSWRInfinite from 'swr/infinite';
-import {FaRegGrinBeamSweat} from 'react-icons/fa'
+import {FaRegGrinBeamSweat} from 'react-icons/fa';
+import {RotatingLines} from 'react-loader-spinner';
+import useIntersectionObserver from './hooks/useintersectionObserver';
 
 const AccessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 const getKey = (pageIndex, previousPageData, query) => {
   if (!query) return null;
   if (previousPageData && previousPageData?.results?.length === 0) return null;
-  return `https://api.unsplash.com/search/photos?client_id=${AccessKey}&page=${pageIndex+1}&query=${query}&per_page=20`
-}
+  return `https://api.unsplash.com/search/photos?client_id=${AccessKey}&page=${
+    pageIndex + 1
+  }&query=${query}&per_page=20`;
+};
 
 const fetcher = async (url) => {
   const res = await fetch(url);
   return res.json();
-}
+};
 
 function App() {
-  const [query, setQuery] = useState("")
+  const ref = useRef();
+  const isIntersecting = useIntersectionObserver(ref);
+  const [query, setQuery] = useState('');
   const onKeyDown = useCallback((event) => {
     if (event.key === 'Enter' && event.target.value.trim().length > 0) {
       setQuery(event.target.value.trim());
     }
-  }, [])
-  
-  const { data, error, setSize, isValidating } = useSWRInfinite((...args) => getKey(...args, query), fetcher, { revalidateFirstPage: false, revalidateOnFocus: false })
-  
-  const photos = data?.map(item => item.results).flat() ?? []
+  }, []);
+
+  const {data, error, setSize, isValidating} = useSWRInfinite((...args) => getKey(...args, query), fetcher, {
+    revalidateFirstPage: false,
+    revalidateOnFocus: false,
+  });
+  console.log(data)
+
+  const photos = data?.map((item) => item.results).flat() ?? [];
   const isEmpty = data?.[0].results?.length === 0;
   const isEnd = photos.length === data?.[0]?.total;
-  const isLoading = (!data && !error && query) || isValidating
+  const isLoading = (!data && !error && query) || isValidating;
+
+  useEffect(() => {
+    if (isIntersecting && !isEnd && !isLoading ) {
+      setSize((oldSize) => oldSize + 1)
+    }
+  }, [isEnd, isIntersecting, isLoading, setSize])
 
 
-  console.log(data)
   return (
     <>
       <div className="input-container">
         <input type="text" placeholder="검색어를 입력해주세요" className="search-input" onKeyDown={onKeyDown}></input>
       </div>
-      {
-        isEmpty ? <div className='no-result'>
+      {isEmpty ? (
+        <div className="no-result">
           <FaRegGrinBeamSweat className="no-result-icon" />
           No Images Found
-        </div> : null
-      }
+        </div>
+      ) : null}
       <div className="image-container">
-        {data?.[0].results.map(({ urls, alt_description }, index) => (
+        {photos.map(({urls, alt_description}, index) => (
           <img key={index} className="image" src={urls?.regular} alt={alt_description} />
-       ))}
+        ))}
+      </div>
+
+      <div ref={ref} className="loading">
+        {isLoading ? (
+          <RotatingLines strokeColor="red" strokeWidth="5" animationDuration="0.75" width="100" visible={true} />
+        ) : isEnd && !isEmpty ? (
+          <p> - - No More Images - - </p>
+        ) : null}
       </div>
     </>
   );
