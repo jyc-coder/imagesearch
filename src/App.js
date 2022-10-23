7792e064e8bdc18895466b8ba904c0a4c34741b3
jyc-coder <jyc4648@gmail.com>
2022-10-23 15:43:08 +0900

    #2 - 이미지 검색 창 구현
    * input에 검색어를 입력하고 `enter`를 치면 `unsplash API`에서 나온 데이터를 바탕으로 이미지가 나타남
    
    * `enter`를 치고 난 다음 `onKeyDown`이 호출되어 `query`값이 변경되면서 `useSWRInfinite`의 `getKey`가 호출되면서 `unsplash API`가 호출됨
    
    https://swr.vercel.app/ko/docs/pagination#useswrinfinite
    
    * 그로 인해 받는 `data`의 `data.[0].results.map`(res로 전송되는 data의 형태가 리스트 내부에 여러개의 객체가 존재하는 형태이며 원하는 데이터는 result에 존재함)을 사용해서 image를 렌더링함
    
    * 검색어의 결과가 존재하지 않는 경우에는 `react-icon`에서 가져온 `FaregGrinBeamSweat`아이콘이 대신 렌더링 되게 설정함
    
    
    * API의 엑세스 키는 보안상 가리기 위해 환경변수를 사용하여 따로 지정함

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import './App.css';
+import { useState, useCallback } from 'react';
+import useSWRInfinite from 'swr/infinite';
+import {FaRegGrinBeamSweat} from 'react-icons/fa'
+
+const AccessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+const getKey = (pageIndex, previousPageData, query) => {
+  if (!query) return null;
+  if (previousPageData && previousPageData?.results?.length === 0) return null;
+  return `https://api.unsplash.com/search/photos?client_id=${AccessKey}&page=${pageIndex+1}&query=${query}&per_page=20`
+}
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  return res.json();
+}
 
 function App() {
+  const [query, setQuery] = useState("")
+  const onKeyDown = useCallback((event) => {
+    if (event.key === 'Enter' && event.target.value.trim().length > 0) {
+      setQuery(event.target.value.trim());
+    }
+  }, [])
+  
+  const { data, error, setSize, isValidating } = useSWRInfinite((...args) => getKey(...args, query), fetcher, { revalidateFirstPage: false, revalidateOnFocus: false })
+  
+  const photos = data?.map(item => item.results).flat() ?? []
+  const isEmpty = data?.[0].results?.length === 0;
+  const isEnd = photos.length === data?.[0]?.total;
+  const isLoading = (!data && !error && query) || isValidating
+
+
+  console.log(data)
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <div className="input-container">
+        <input type="text" placeholder="검색어를 입력해주세요" className="search-input" onKeyDown={onKeyDown}></input>
+      </div>
+      {
+        isEmpty ? <div className='no-result'>
+          <FaRegGrinBeamSweat className="no-result-icon" />
+          No Images Found
+        </div> : null
+      }
+      <div className="image-container">
+        {data?.[0].results.map(({ urls, alt_description }, index) => (
+          <img key={index} className="image" src={urls?.regular} alt={alt_description} />
+       ))}
+      </div>
+    </>
   );
 }
 
